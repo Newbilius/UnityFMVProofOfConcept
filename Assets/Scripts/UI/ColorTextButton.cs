81abea3a0c1783fcb65671f58268ae762dd13fee
newbilius <newbilius@gmail.com>
2020-06-26 23:03:45 +0500

    Поправил странность, когда визуально выбиралось сразу два элемента

--- Assets/Scripts/UI/ColorTextButton.cs
+++ Assets/Scripts/UI/ColorTextButton.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 //todo нужно сделать так, что изменения в редакторе тут же перерисовывались
@@ -15,20 +17,40 @@ public class ColorTextButton : Button
         Text = GetComponentInChildren<Text>();
     }
 
+    private bool IsPointerInside { get; set; }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        base.OnPointerEnter(eventData);
+        Select();
+        IsPointerInside = true;
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        base.OnPointerExit(eventData);
+        IsPointerInside = false;
+    }
+
+    void OnGUI()
+    {
+        var mouseValue = Mouse.current.delta.ReadValue();
+        var mouseDelta = Mathf.Abs(mouseValue.x) + Mathf.Abs(mouseValue.y);
+        if (mouseDelta > 2 && IsPointerInside)
+            Select();
+    }
+
     protected override void DoStateTransition(SelectionState state, bool instant)
     {
         switch (state)
         {
-            case SelectionState.Normal:
-                Text.fontSize = NormalFontSize;
-                break;
-
-            case SelectionState.Highlighted:
-                Select();
-                break;
-
+            case SelectionState.Pressed:
             case SelectionState.Selected:
                 Text.fontSize = SelectedFontSize;
+                break;
+
+            default:
+                Text.fontSize = NormalFontSize;
                 break;
         }
     }
